caf626bdba4bad9a7bb5e4021594e3b6b8cee1ec
Daniele Cavalcante <39858223+daneccf@users.noreply.github.com>
2019-12-02 21:42:31 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Chess validator in pgn format
 
 Programa escrito em C que recebe um arquivo .pgn na linha de comando. Esse arquivo deve conter todas as jogadas de uma partida de xadrez. O programa verifica se cada movimento realizado é válido ou não, atualizando o tabuleiro sempre que uma peça mover corretamente.
+
+Para compilar, comando:
+make
+
+Para executar, comando:
+./main nomedoarquivo.pgn
+
+Em caso de mensagem "permissao negada", utilizo o comando: "chmod 777 main" e executo a main de novo.
--- main.c
+++ main.c
@@ -1,3 +1,16 @@
+/*
+Codigo escrito em C para a disciplina de Programacao 2019.2 feito por:
+
+Daniele Cavalcante Campos Frota - 414624
+Danilo Carneiro Freire de Castro - 470077
+
+Inicialmente, trabalhamos em conjunto na criação da estrutura de dados e leitura do arquivo, bem como esse seria tratado.
+No decorrer das necessidades, fomos definindo as funcoes basicas que deveriam ser implementadas para inciar os testes e assim avancando para casos mais complexos.
+Primeiro foi pensado em uma logica que os dois deveriam seguir pra programar semelhante nas funcoes de percorrer as direcoes e mover pecas.
+Cada um foi pegando uma funcao ainda nao implementada. Nos casos de falhas de segmentacao ou movimentos inesperados, nos reuniamos no LEC para discutir e corrigir.
+Ou seja, as funcoes foram bem divididas para os dois. A parte final de modularizar, criar o Makefile e documentar tambem foi feito em conjunto.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
